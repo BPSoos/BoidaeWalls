18c6a26b818f37f998c461da28fff29475b9d361
BPSoos <soos.peter.96@gmail.com>
2020-08-20 21:35:20 +0200

    initial commit, unity project created

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public PlayerUI playerClass;
+    public List<PlayerUI> addedPlayersList;
+    private int first_empty_slot = 0;
+    public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex  + 1);
+    }
+
+    void Start(){
+        for (int i = 0; i < 8; i++){        
+        AddPlayer();
+        }
+    }
+
+    public void QuitGame(){
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void AddPlayer(){
+        PlayerUI new_player_object = Instantiate(playerClass);
+        new_player_object.transform.SetParent(this.transform, false);
+        new_player_object.UpdatePosition(addedPlayersList.Count);
+        new_player_object.player_count = addedPlayersList.Count;
+        addedPlayersList.Add(new_player_object);
+    }
+
+    public void AddPlayerButton(){
+        for (int i = 0; i < 8; i++){
+            if (!(addedPlayersList[i].gameObject.activeSelf) &&
+                            (addedPlayersList[i].player_y_slot == first_empty_slot)){
+                addedPlayersList[i].gameObject.SetActive(true);
+            }
+        }        
+        first_empty_slot ++;
+        Debug.Log("first_empty_slot: " + first_empty_slot.ToString());
+    }
+
+    public void ChooseColor(string color_chosen){
+    Debug.Log(color_chosen);
+    }
+
+    public void PlayerRemoved(int player_slot, int player_count){
+        int last_slot = player_slot;
+        for (int i = 0; i < 8; i++){
+            if (addedPlayersList[i].player_y_slot > player_slot){
+                 addedPlayersList[i].UpdatePosition(addedPlayersList[i].player_y_slot - 1);
+            }
+        }
+        addedPlayersList[player_count].UpdatePosition(7);
+        first_empty_slot --;
+        Debug.Log("first_empty_slot: " + first_empty_slot.ToString());
+
+    }
+}
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{   
+    public GameObject[] playerPrefabs = new GameObject[4];
+    public int player_y_slot {get; set;}
+    public int player_count {get; set;}
+
+    // Start is called before the first frame update
+
+    void Start(){
+        transform.gameObject.SetActive(false);
+        InitializeButtons();
+    }
+
+    public void CreateUIElements()
+    {
+        Debug.Log("a");
+    }
+
+    public void UpdatePosition(int new_slot){
+        foreach (Transform player_item in transform){
+            Vector3 new_position = new Vector3(player_item.transform.position.x, 500 - new_slot * 40 , 0);
+            player_item.position = new_position;
+            player_y_slot = new_slot;
+        }
+    }
+
+    public void RemoveButton(){
+        transform.gameObject.SetActive(false);
+        transform.parent.GetComponent<MainMenu>().PlayerRemoved(player_y_slot, player_count);
+    }
+
+    public void InitializeButtons(){
+        for (int i = 0; i < playerPrefabs.Length; i++){
+            if (playerPrefabs[i].tag.Contains("ExitButton")){
+                Button button = playerPrefabs[i].GetComponent<Button>();
+                button.onClick.AddListener( ()=> RemoveButton() );
+            }
+        }
+    }
+}
